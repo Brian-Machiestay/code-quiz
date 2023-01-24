df6619003c42466776585f1c04ac977c1a215e8b
Brian-Machiestay <brianmachiestay@gmail.com>
2023-01-24 15:01:59 +0000

    seperate logic and get dom objects

---
+++ assets/js/logic.js
@@ -0,0 +1,6 @@
+// startScreen obj
+startScreenWrapper = document.getElementById("start-screen");
+
+// questions object
+questionsDiv = document.getElementById("questions");
+questionsDiv.setAttribute("class", "show");
---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+let questions = [
+    {
+        title: "Which of the following is not a javascript data type",
+        choices: ["Number", "Array", "string", "List"],
+        answer: "List",
+    },
+    {
+        title: "What is the full meaning of DOM",
+        choices: ["Dawn of Monday", "Document Object Moment", "Document Object Module", "Document Object Model"],
+        answer: "Document Object Model",
+    },
+    {
+        title: "One of the following is not a javascript framework",
+        choices: ["Jquery", "Angular", "Moment", "Flask"],
+        answer: "flask",
+    }
+    ]
